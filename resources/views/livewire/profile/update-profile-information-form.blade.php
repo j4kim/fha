4be4ea2b318e7483a4bbd54eb6f9a5f34bc651ec
Joaquim Perez <jivkim@gmail.com>
2024-09-08 14:46:11 +0200

    update-profile-information-form: remove email verification stuff

--- resources/views/livewire/profile/update-profile-information-form.blade.php
+++ resources/views/livewire/profile/update-profile-information-form.blade.php
@@ -32,23 +32,6 @@ $updateProfileInformation = function () {
 
     $this->dispatch('profile-updated', name: $user->name);
 };
-
-$sendVerification = function () {
-    $user = Auth::user();
-
-    if ($user->hasVerifiedEmail()) {
-        $path = session('url.intended', RouteServiceProvider::HOME);
-
-        $this->redirect($path);
-
-        return;
-    }
-
-    $user->sendEmailVerificationNotification();
-
-    Session::flash('status', 'verification-link-sent');
-};
-
 ?>
 
 <section>
@@ -73,24 +56,6 @@ $sendVerification = function () {
             <x-input-label for="email" :value="__('Email')" />
             <x-text-input wire:model="email" id="email" name="email" type="email" class="mt-1 block w-full" required autocomplete="username" />
             <x-input-error class="mt-2" :messages="$errors->get('email')" />
-
-            @if (auth()->user() instanceof MustVerifyEmail && ! auth()->user()->hasVerifiedEmail())
-                <div>
-                    <p class="text-sm mt-2 text-gray-800">
-                        {{ __('Your email address is unverified.') }}
-
-                        <button wire:click.prevent="sendVerification" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                            {{ __('Click here to re-send the verification email.') }}
-                        </button>
-                    </p>
-
-                    @if (session('status') === 'verification-link-sent')
-                        <p class="mt-2 font-medium text-sm text-green-600">
-                            {{ __('A new verification link has been sent to your email address.') }}
-                        </p>
-                    @endif
-                </div>
-            @endif
         </div>
 
         <div class="flex items-center gap-4">

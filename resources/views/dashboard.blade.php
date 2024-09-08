a62716a82cb727b0fba1011006fd92ef98121cd2
Joaquim Perez <jivkim@gmail.com>
2024-09-08 18:23:53 +0200

    refactor(layout)

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,9 +5,5 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            Hello
-        </div>
-    </div>
+    Hello
 </x-app-layout>

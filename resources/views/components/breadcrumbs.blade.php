99293bcb7699ef07d5e68b74f4ad5db66b272b63
Joaquim Perez <jivkim@gmail.com>
2024-09-16 22:57:20 +0200

    move svg icons in components

--- resources/views/components/breadcrumbs.blade.php
+++ resources/views/components/breadcrumbs.blade.php
@@ -5,9 +5,7 @@
         <x-breadcrumb :item="$item"/>
 
         @unless($loop->last)
-            <svg class="fill-current h-3 sm:h-4" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" clip-rule="evenodd" d="M7.3,14.7c-0.4-0.4-0.4-1,0-1.4l3.3-3.3L7.3,6.7c-0.4-0.4-0.4-1,0-1.4s1-0.4,1.4,0c0,0,0,0,0,0l4,4c0.4,0.4,0.4,1,0,1.4l-4,4C8.3,15.1,7.7,15.1,7.3,14.7z"/>
-            </svg>
+            <x-icons.chevron class="h-3 sm:h-4" />
         @endunless
     @endforeach
 </div>
---
+++ resources/views/components/icons/chevron.blade.php
@@ -0,0 +1,3 @@
+<svg {{ $attributes->merge(['class' => 'fill-current']) }} viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M7.3,14.7c-0.4-0.4-0.4-1,0-1.4l3.3-3.3L7.3,6.7c-0.4-0.4-0.4-1,0-1.4s1-0.4,1.4,0c0,0,0,0,0,0l4,4c0.4,0.4,0.4,1,0,1.4l-4,4C8.3,15.1,7.7,15.1,7.3,14.7z"/>
+</svg>

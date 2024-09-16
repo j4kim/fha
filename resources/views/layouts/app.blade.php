cfb6ee9ee2be31a6361731e23cd86aba9bab8bac
Joaquim Perez <jivkim@gmail.com>
2024-09-16 23:34:28 +0200

    layout(profile)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,7 +32,7 @@
 
             <!-- Page Content -->
             <main>
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 pb-36">
                     {{ $slot }}
                 </div>
             </main>

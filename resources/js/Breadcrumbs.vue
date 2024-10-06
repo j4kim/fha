53ba672598fef2ea02e133f48bd7043da268eb0a
Joaquim Perez <jivkim@gmail.com>
2024-10-06 20:13:33 +0200

    feat(Breadcrumbs): don't render link for last item

--- resources/js/Breadcrumbs.vue
+++ resources/js/Breadcrumbs.vue
@@ -1,5 +1,4 @@
 <script setup>
-import { useRoute } from "vue-router";
 import ChevronIcon from "./ChevronIcon.vue";
 import store from "./store";
 </script>
@@ -7,17 +6,19 @@ import store from "./store";
 <template>
     <div class="flex gap-1 sm:gap-2 gap-y-0 items-center flex-wrap">
         <template v-for="(breadcrumb, i) in store.breadcrumbs">
-            <RouterLink
-                :to="breadcrumb.route"
-                class="text-sm sm:text-lg text-indigo-700"
-            >
-                {{ breadcrumb.text }}
-            </RouterLink>
+            <template v-if="i < store.breadcrumbs.length - 1">
+                <RouterLink
+                    :to="breadcrumb.route"
+                    class="text-sm sm:text-lg text-indigo-700"
+                >
+                    {{ breadcrumb.text }}
+                </RouterLink>
+                <ChevronIcon class="h-3 sm:h-4" />
+            </template>
 
-            <ChevronIcon
-                v-if="i < store.breadcrumbs.length - 1"
-                class="h-3 sm:h-4"
-            />
+            <h1 v-else class="text-sm sm:text-lg">
+                {{ breadcrumb.text }}
+            </h1>
         </template>
     </div>
 </template>

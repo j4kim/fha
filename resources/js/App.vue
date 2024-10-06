a7a4877ed3163cad05c314eb3ebec54796dca5df
Joaquim Perez <jivkim@gmail.com>
2024-10-06 20:03:06 +0200

    App.vue: remove loading text

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -26,7 +26,6 @@ import store from "./store";
         <!-- Page Content -->
         <main>
             <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 pb-36">
-                <div v-if="store.loading">Chargement...</div>
                 <Suspense>
                     <RouterView />
                 </Suspense>

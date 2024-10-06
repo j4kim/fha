9956f3d851f3cb468243419bf7fb5c57f2cabee3
Joaquim Perez <jivkim@gmail.com>
2024-10-07 01:36:57 +0200

    enable sanctum

--- resources/js/api.js
+++ resources/js/api.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 import store from "./store";
 
+axios.defaults.withCredentials = true;
+axios.defaults.withXSRFToken = true;
+
 export async function get(uri) {
     store.loading = true;
     const response = await axios.get(uri);

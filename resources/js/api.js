50d2316ec000c2b586952a64d9632037ceb58d90
Joaquim Perez <jivkim@gmail.com>
2024-12-01 17:55:59 +0100

    feat(api.js): new post method

--- resources/js/api.js
+++ resources/js/api.js
@@ -4,9 +4,17 @@ import store from "./store";
 axios.defaults.withCredentials = true;
 axios.defaults.withXSRFToken = true;
 
-export async function get(uri) {
+export async function api(uri, method = "get", data = {}) {
     store.loading = true;
-    const response = await axios.get(uri);
+    const response = await axios(uri, { method, data });
     store.loading = false;
     return response.data;
+}
+
+export async function get(uri) {
+    return api(uri);
+}
+
+export async function post(uri, data) {
+    return api(uri, "post", data);
 }
